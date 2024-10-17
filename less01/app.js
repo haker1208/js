fa90fb68a875ea02f3c2d2dcf6d10a69d48ccc88
haker1208 <halimastiv@gmail.com>
2024-10-17 17:50:30 +0300

    Update app.js

--- less01/app.js
+++ less01/app.js
@@ -1,5 +1,5 @@
-const myName = "Halima";
+const myName = 'Halima';
 const myAge = 15;
-const myCountry = "Russian';
+const myCountry = 'Russian';
 
 console.log(myName + ', ' + myAge + ', ' + myCountry);
