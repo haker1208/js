e0c174cb3e9ec0756f634ab5de01a20bac5ba8e6
haker1208 <halimastiv@gmail.com>
2024-10-17 17:39:11 +0300

    sozdali js

---
+++ less01/app.js
@@ -0,0 +1,5 @@
+const myName = "Halima";
+const myAge = 15;
+const myCountry = "Russian';
+
+console.log(myName + ', ' + myAge + ', ' + myCountry);
---
+++ less02/app.js
@@ -0,0 +1,26 @@
+let strong = "Halima";
+console.log(typeof strong);
+
+let number = 123;
+console.log(typeof number);
+
+let bigint = 569543678909876543;
+console.log(typeof bigint);
+
+let boolen = true;
+console.log(typeof boolen);
+
+
+let email;
+console.log(typeof email);
+
+const user = {};
+console.log(typeof user);
+
+let nll = null;
+console.log(typeof nll);
+
+
+
+
+
